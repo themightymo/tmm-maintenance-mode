7e8e9e88fa298d710fbdebc8ba82d8a0096ee9bb
Toby Cryns <themightymo@users.noreply.github.com>
2020-05-18 18:58:12 -0500

    Added Admin login & new logo

--- tmm-maintenance-mode.php
+++ tmm-maintenance-mode.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * Plugin Name: Maintenance Mode by The Mighty Mo! Design Co.
  * Plugin URI: http://www.themightymo.com/
@@ -6,12 +7,12 @@
  * Author: The Mighty Mo! Design Co. LLC
  * Author URI: http://www.themightymo.com/
  * License: GPLv2 (or later)
- * Version: 1.2
+ * Version: 1.3
  */
  
 function tmm_maintenance_mode() {
     if ( !current_user_can( 'edit_themes' ) || !is_user_logged_in() ) {
-        wp_die('<center><img src="http://www.themightymo.com/wp-content/uploads/2013/06/the-mighty-mo-logo.png" /><br>We are building stuff behind the scenes!  Please come back soon!</center>');
+        wp_die('<center><img src="https://www.themightymo.com/wp-content/uploads/2018/03/the-mighty-mo-logo-March-2018-green-200px-new.png" /><br>We are building stuff behind the scenes!  Please come back soon!</center><p><center><a href="/wp-login.php">Admin Login</a></center></p>');
     }
 }
 add_action('get_header', 'tmm_maintenance_mode');
