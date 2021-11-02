5656f1a9b8ed06f15186b805a0c9664e2ff23521
Toby Cryns <themightymo@users.noreply.github.com>
2021-11-02 11:02:05 -0500

    Update tmm-maintenance-mode.php

--- tmm-maintenance-mode.php
+++ tmm-maintenance-mode.php
@@ -7,7 +7,7 @@
  * Author: The Mighty Mo! Design Co. LLC
  * Author URI: https://www.themightymo.com/
  * License: GPLv2 (or later)
- * Version: 1.7
+ * Version: 1.8
  * GitHub Branch: master
  * GitHub Plugin URI: themightymo/tmm-maintanence-mode
  * GitHub Plugin URI: https://github.com/themightymo/tmm-maintanence-mode
